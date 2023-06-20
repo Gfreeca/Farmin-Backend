ca5eec9f951a447efe13ebf5196c3f15f365c2b5
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-20 17:20:32 +0900

    :sparkles: Base id entity 생성

---
+++ src/main/kotlin/team/kimfarmer/farmin/global/common/entity/BaseIdEntity.kt
@@ -0,0 +1,13 @@
+package team.kimfarmer.farmin.global.common.entity
+
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseIdEntity(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long = 0
+)

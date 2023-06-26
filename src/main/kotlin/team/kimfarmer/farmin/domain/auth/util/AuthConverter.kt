fd06aa992b1393dd979a38730c04d3bf3360072f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-26 14:05:37 +0900

    :sparkles: Converter 구현

--- src/main/kotlin/team/kimfarmer/farmin/domain/auth/util/AuthConverter.kt
+++ src/main/kotlin/team/kimfarmer/farmin/domain/auth/util/AuthConverter.kt
@@ -1,15 +1,21 @@
 package team.kimfarmer.farmin.domain.auth.util
 
 import team.kimfarmer.farmin.domain.auth.domain.entity.RefreshToken
+import team.kimfarmer.farmin.domain.auth.presentation.data.dto.SignInDto
+import team.kimfarmer.farmin.domain.auth.presentation.data.dto.SignInTokenDto
 import team.kimfarmer.farmin.domain.auth.presentation.data.dto.SignUpDto
 import team.kimfarmer.farmin.domain.auth.presentation.data.dto.SignUpTokenDto
+import team.kimfarmer.farmin.domain.auth.presentation.data.request.SignInRequestDto
 import team.kimfarmer.farmin.domain.auth.presentation.data.request.SignUpRequestDto
+import team.kimfarmer.farmin.domain.auth.presentation.data.response.SignInResponseDto
 import team.kimfarmer.farmin.domain.auth.presentation.data.response.SignUpResponseDto
 import team.kimfarmer.farmin.domain.user.domain.entity.User
 
 interface AuthConverter {
     fun toDto(request: SignUpRequestDto): SignUpDto
+    fun toDto(request: SignInRequestDto): SignInDto
     fun toResponse(dto: SignUpTokenDto): SignUpResponseDto
+    fun toResponse(dto: SignInTokenDto): SignInResponseDto
     fun toEntity(token: String, id: String, timeToLive: Long): RefreshToken
     fun toEntity(dto: SignUpDto, encodedPassword: String): User
 }

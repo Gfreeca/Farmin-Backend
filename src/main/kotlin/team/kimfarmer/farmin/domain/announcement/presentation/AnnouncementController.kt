85d11f4d422bac03e6b69bf44345381a7decc1e6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-27 18:04:52 +0900

    :sparkles: Converter 생성

--- src/main/kotlin/team/kimfarmer/farmin/domain/announcement/presentation/AnnouncementController.kt
+++ src/main/kotlin/team/kimfarmer/farmin/domain/announcement/presentation/AnnouncementController.kt
@@ -1,5 +1,11 @@
 package team.kimfarmer.farmin.domain.announcement.presentation
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,14 +14,34 @@ import org.springframework.web.bind.annotation.RestController
 import team.kimfarmer.farmin.domain.announcement.presentation.data.response.AnnouncementResponseDto
 import team.kimfarmer.farmin.domain.announcement.service.FindAnnouncementsService
 import team.kimfarmer.farmin.domain.announcement.utils.AnnouncementConverter
+import team.kimfarmer.farmin.domain.auth.presentation.data.response.SignUpResponseDto
 
 @RestController
+@Tag(name = "announcement", description = "공고 API")
 @RequestMapping("/announcement")
 class AnnouncementController(
         private val findAnnouncementsService: FindAnnouncementsService,
         private val announcementConverter: AnnouncementConverter
 ) {
     @GetMapping
+    @Operation(summary = "지원하기", description = "인력공고에 지원 신청하기")
+    @ApiResponses(
+            value = [
+                ApiResponse(
+                        responseCode = "200", description = "농촌 공고리스트 불러오기 성공",
+                        content = [Content(schema = Schema(implementation = SignUpResponseDto::class))]
+                ),
+                ApiResponse(
+                        responseCode = "401", description = "권한이 없는 경우",
+                        content = [Content(schema = Schema(implementation = String::class))]
+                ),
+                ApiResponse(
+                        responseCode = "404", description = "존재하지 않는 농장",
+                        content = [Content(schema = Schema(implementation = String::class))]
+                )
+
+            ]
+    )
     fun findAnnouncements(): ResponseEntity<List<AnnouncementResponseDto>> =
             findAnnouncementsService.execute()
                     .map { announcementConverter.toResponse(it) }
--- src/main/kotlin/team/kimfarmer/farmin/domain/announcement/utils/impl/AnnouncementConverterImpl.kt
+++ src/main/kotlin/team/kimfarmer/farmin/domain/announcement/utils/impl/AnnouncementConverterImpl.kt
@@ -1,12 +1,23 @@
 package team.kimfarmer.farmin.domain.announcement.utils.impl
 
 import org.springframework.stereotype.Component
+import team.kimfarmer.farmin.domain.announcement.domain.entity.Announcement
 import team.kimfarmer.farmin.domain.announcement.presentation.data.dto.AnnouncementDto
 import team.kimfarmer.farmin.domain.announcement.presentation.data.response.AnnouncementResponseDto
 import team.kimfarmer.farmin.domain.announcement.utils.AnnouncementConverter
+import team.kimfarmer.farmin.domain.farm.domain.entity.Farm
 
 @Component
 class AnnouncementConverterImpl : AnnouncementConverter {
+    override fun toDto(announcement: Announcement, farm: Farm): AnnouncementDto =
+            AnnouncementDto(
+                    thumbnail = announcement.thumbnail,
+                    name = announcement.name,
+                    pay = announcement.pay,
+                    location = farm.location,
+                    farmName = farm.name
+            )
+
     override fun toResponse(dto: AnnouncementDto): AnnouncementResponseDto =
             AnnouncementResponseDto(
                     thumbnail = dto.thumbnail,

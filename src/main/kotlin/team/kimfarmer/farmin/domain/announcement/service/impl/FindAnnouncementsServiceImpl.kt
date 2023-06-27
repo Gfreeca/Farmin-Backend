4c1d80339a9de5255db17d9b5287d7e24d871f92
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-27 16:13:19 +0900

    :recycle: 로직 수정
    farmUtils 적용

--- src/main/kotlin/team/kimfarmer/farmin/domain/announcement/service/impl/FindAnnouncementsServiceImpl.kt
+++ src/main/kotlin/team/kimfarmer/farmin/domain/announcement/service/impl/FindAnnouncementsServiceImpl.kt
@@ -1,23 +1,22 @@
 package team.kimfarmer.farmin.domain.announcement.service.impl
 
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.kimfarmer.farmin.domain.announcement.domain.repository.AnnouncementRepository
 import team.kimfarmer.farmin.domain.announcement.presentation.data.dto.AnnouncementDto
 import team.kimfarmer.farmin.domain.announcement.service.FindAnnouncementsService
 import team.kimfarmer.farmin.domain.announcement.utils.AnnouncementConverter
-import team.kimfarmer.farmin.domain.farm.domain.repository.FarmRepository
+import team.kimfarmer.farmin.domain.farm.utils.FarmUtils
 
 @Service
 @Transactional(readOnly = true)
 class FindAnnouncementsServiceImpl(
         private val announcementRepository: AnnouncementRepository,
-        private val farmRepository: FarmRepository,
+        private val farmUtils: FarmUtils,
         private val announcementConverter: AnnouncementConverter
 ) : FindAnnouncementsService {
     override fun execute(): List<AnnouncementDto> =
          announcementRepository.findAll()
-                .map { it to farmRepository.findByIdOrNull(it.farmIdx) }
-                .map { announcementConverter.toDto(it.first, it.second!!) }
+                .map { it to farmUtils.findByFarmIdx(it.farmIdx) }
+                .map { announcementConverter.toDto(it.first, it.second) }
 }

04ea32b90b164e7afbe1d28f0c8d96ebf558fd56
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-27 15:19:45 +0900

    :sparkles: 컨트롤러 구현

--- src/main/kotlin/team/kimfarmer/farmin/domain/announcement/presentation/AnnouncementController.kt
+++ src/main/kotlin/team/kimfarmer/farmin/domain/announcement/presentation/AnnouncementController.kt
@@ -1,9 +1,23 @@
 package team.kimfarmer.farmin.domain.announcement.presentation
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import team.kimfarmer.farmin.domain.announcement.presentation.data.response.AnnouncementResponseDto
+import team.kimfarmer.farmin.domain.announcement.service.FindAnnouncementsService
+import team.kimfarmer.farmin.domain.announcement.utils.AnnouncementConverter
 
 @RestController
 @RequestMapping("/announcement")
-class AnnouncementController {
+class AnnouncementController(
+        private val findAnnouncementsService: FindAnnouncementsService,
+        private val announcementConverter: AnnouncementConverter
+) {
+    @GetMapping
+    fun findAnnouncements(): ResponseEntity<List<AnnouncementResponseDto>> =
+            findAnnouncementsService.execute()
+                    .map { announcementConverter.toResponse(it) }
+                    .let { ResponseEntity.status(HttpStatus.OK).body(it) }
 }

933e602eed9c36df9e5529c0c77e780060b9c431
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-29 11:55:09 +0900

    :sparkles: 컨트롤러 생성

--- src/main/kotlin/team/kimfarmer/farmin/domain/announcement/presentation/AnnouncementController.kt
+++ src/main/kotlin/team/kimfarmer/farmin/domain/announcement/presentation/AnnouncementController.kt
@@ -9,9 +9,11 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.kimfarmer.farmin.domain.announcement.presentation.data.response.AnnouncementResponseDto
+import team.kimfarmer.farmin.domain.announcement.presentation.data.response.DetailAnnouncementResponseDto
 import team.kimfarmer.farmin.domain.announcement.service.FindAnnouncementsService
 import team.kimfarmer.farmin.domain.announcement.utils.AnnouncementConverter
 import team.kimfarmer.farmin.domain.auth.presentation.data.response.SignUpResponseDto
@@ -46,4 +48,8 @@ class AnnouncementController(
             findAnnouncementsService.execute()
                     .map { announcementConverter.toResponse(it) }
                     .let { ResponseEntity.status(HttpStatus.OK).body(it) }
+    @GetMapping("/{announcement_id}")
+    fun findAnnouncementById(@PathVariable("announcement_id") announcementId: String): ResponseEntity<DetailAnnouncementResponseDto> {
+        return TODO("서비스 구현하고 로직 수정하기")
+    }
 }

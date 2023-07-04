0574cb52293a21b5372d5e3b4e8f4b16e1db5060
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-07-04 20:17:44 +0900

    :recycle: 에러 코드 수정

--- src/main/kotlin/team/kimfarmer/farmin/domain/announcement/exception/AnnouncementNotFoundException.kt
+++ src/main/kotlin/team/kimfarmer/farmin/domain/announcement/exception/AnnouncementNotFoundException.kt
@@ -3,4 +3,4 @@ package team.kimfarmer.farmin.domain.announcement.exception
 import team.kimfarmer.farmin.global.exception.ErrorCode
 import team.kimfarmer.farmin.global.exception.exceptions.BasicException
 
-class AnnouncementNotFoundException : BasicException(ErrorCode.USER_NOT_FOUND)
+class AnnouncementNotFoundException : BasicException(ErrorCode.ANNOUNCEMENT_NOT_FOUND)
